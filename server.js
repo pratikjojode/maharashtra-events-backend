bb5459864fb6943b5e45dcf3362d906405306b5f
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-04-18 11:08:00 +0530

    okk

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ import dotenv from "dotenv";
 import colors from "colors";
 import formRoutes from "./routes/formRoutes.js";
 import morgan from "morgan";
+
 dotenv.config();
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
+
+// Health check route
+app.get("/", (req, res) => {
+  res.send("API is working 🚀");
+});
 
 app.use("/api/v1/forms", formRoutes);
 
